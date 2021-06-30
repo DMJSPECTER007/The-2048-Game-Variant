5989020297e6b962471f1bd4d4260aa2db8afc52
Dheer Manish Jain <dheerjain000@gmail.com>
2021-06-30 11:27:02 +0530

    Update game.c

--- game.c
+++ game.c
@@ -2,7 +2,7 @@
 #include<time.h>
 #include<stdlib.h>
 #include<string.h>
-#define SIZE 4
+#define SIZE 4     // it is a 4 * 4 matrix
 int board[SIZE][SIZE];   //Game Board
 int generateRandom(int low,int high)    // generate a random number in the range [low,high]
 {
@@ -15,26 +15,26 @@ void introMessage()
 }
 void randomFill()      //insert a random num(2/4) at an empty cell in the board
 {
-	int occupied=1;
+	int occupied=1;     // if the randomly chosen cell is occupied or not
 	while(occupied)
 	{
-		int cell=generateRandom(0,15);
-		if(board[cell/SIZE][cell%SIZE]==0)
+		int cell=generateRandom(0,15);  //since its a 4*4 board, we have 16 cells. so we choose randomly from 0 to 15.
+		if(board[cell/SIZE][cell%SIZE]==0)  // cell/SIZE gives the row index and cell%SIZE gives me the column index
 		{
 			while(1)
 			{
-				int num=generateRandom(0,4);
-				if(num==2||num==4)
+				int num=generateRandom(0,4); //since we need either 2 or 4, we take the range 0 to 4
+				if(num==2||num==4)     // we fill the empty cell
 				{
 					board[cell/SIZE][cell%SIZE]=num;
 					break;
 				}
 			}
-			occupied=0;
+			occupied=0;    //Since the cell was initially empty, we set occupied as false
 		}
 	}
 }
-int compare(int a[SIZE][SIZE],int b[SIZE][SIZE])
+int compare(int a[SIZE][SIZE],int b[SIZE][SIZE])   //comparison between two states
 {
 	for(int i=0; i<SIZE; i++)
 	{
@@ -55,37 +55,38 @@ void startGame()
 	{
 		for(int j=0; j<4; j++)
 		{
-			board[i][j]=0;
+			board[i][j]=0;    //0 indicates an empty cell
 		}
 	}
-	randomFill();
+	//since initially, we have two random filled cells, we do two random fillings.
+	randomFill();      
 	randomFill();
 
 }
-void leftMove(char op)  
+void leftMove(char op)  //covering every possible left move variants
 {
-	int old[SIZE][SIZE];
+	int old[SIZE][SIZE];  //state of the matrix before the operation
 	for(int i=0; i<SIZE; i++)
 	{
 		for(int j=0; j<SIZE; j++)
 		{
-			old[i][j]=board[i][j];
+			old[i][j]=board[i][j];   
 		}
 	}
-	for(int i=0; i<SIZE; i++)
+	for(int i=0; i<SIZE; i++)   // we are going row by row
 	{
-		int a[SIZE];
+		int a[SIZE];   // this will be the update row after the operation
 		for(int j=0; j<SIZE; j++)
 		{
 			a[j]=0;
 		}
-		int k=0;
-		int flip=0;
+		int k=0;   //pointer to element that has to be filled in updated row
+		int isOp=0;     //whether a add/sub/mul/div operation has been performed or not
 		for(int j=0; j<SIZE; j++)
 		{
-			if(board[i][j]!=0)
+			if(board[i][j]!=0)  // non empty
 			{
-				if(k>0 && board[i][j]==a[k-1] && flip==0)
+				if(k>0 && board[i][j]==a[k-1] && isOp==0)  //operation is performed here
 				{
 
 					if(op=='+')
@@ -94,24 +95,24 @@ void leftMove(char op)
 						a[k-1]=0;
 					else if(op=='*')
 						a[k-1]*=a[k-1];
-					else
+					else                         //division
 						a[k-1]=1;
-					flip=1;
+					isOp=1;   //the operation has been performed, so no further operations allowed.
 				}
-				else
+				else    //normal fillup
 				{
 					a[k]=board[i][j];
 					k++;
-					flip=0;
+					isOp=0;
 				}
 			}
 		}
-		for(int j=0; j<SIZE; j++)
+		for(int j=0; j<SIZE; j++)   //updating the board row after the operation
 		{
 			board[i][j]=a[j];
 		}
 	}
-	if(!compare(old,board))
+	if(!compare(old,board))   //if board state as changed, we insert a random tile
 		randomFill();
 
 }
@@ -133,13 +134,13 @@ void rightMove(char op)
 			a[j]=0;
 		}
 		int k=SIZE-1;
-		int flip=0;
+		int isOp=0;
 
 		for(int j=SIZE-1; j>=0; j--)
 		{
 			if(board[i][j]!=0)
 			{
-				if(k<SIZE-1 && board[i][j]==a[k+1] && flip==0)
+				if(k<SIZE-1 && board[i][j]==a[k+1] && isOp==0)
 				{
 					if(op=='+')
 						a[k+1]*=2;
@@ -149,13 +150,13 @@ void rightMove(char op)
 						a[k+1]*=a[k+1];
 					else
 						a[k+1]=1;
-					flip++;
+					isOp++;
 				}
 				else
 				{
 					a[k]=board[i][j];
 					k--;
-					flip=0;
+					isOp=0;
 				}
 			}
 		}
@@ -185,12 +186,12 @@ void upMove(char op)
 			a[j]=0;
 		}
 		int k=0;
-		int flip=0;
+		int isOp=0;
 		for(int j=0; j<SIZE; j++)
 		{
 			if(board[j][i]!=0)
 			{
-				if(k>0 && board[j][i]==a[k-1] && flip==0)
+				if(k>0 && board[j][i]==a[k-1] && isOp==0)
 				{
 					if(op=='+')
 						a[k-1]*=2;
@@ -202,13 +203,13 @@ void upMove(char op)
 						a[k-1]*=a[k-1];
 					else
 						a[k-1]=1;
-					flip++;
+					isOp++;
 				}
 				else
 				{
 					a[k]=board[j][i];
 					k++;
-					flip=0;
+					isOp=0;
 				}
 			}
 		}
@@ -238,12 +239,12 @@ void downMove(char op)
 			a[j]=0;
 		}
 		int k=SIZE-1;
-		int flip=0;
+		int isOp=0;
 		for(int j=SIZE-1; j>=0; j--)
 		{
 			if(board[j][i]!=0)
 			{
-				if(k<SIZE-1 && board[j][i]==a[k+1] && flip==0)
+				if(k<SIZE-1 && board[j][i]==a[k+1] && isOp==0)
 				{
 					if(op=='+')
 						a[k+1]*=2;
@@ -255,13 +256,13 @@ void downMove(char op)
 						a[k+1]*=a[k+1];
 					else
 						a[k+1]=1;
-					flip++;
+					isOp++;
 				}
 				else
 				{
 					a[k]=board[j][i];
 					k--;
-					flip=0;
+					isOp=0;
 				}
 			}
 		}
@@ -273,7 +274,7 @@ void downMove(char op)
 	if(!compare(old,board))
 		randomFill();
 }
-void printBoard()
+void printBoard()   //printing the state of the board
 {
 	for(int i=0; i<SIZE; i++)
 	{
@@ -287,7 +288,7 @@ void printBoard()
 		printf("\n");
 	}
 }
-int winState()
+int winState()    //checking if we have got a 2048 tile(the winning mark)
 {
 	for(int i=0; i<SIZE; i++)
 	{
@@ -310,8 +311,8 @@ int main()
 	printf("\n");
 	while(1)
 	{
-		char command[100];
-		fgets(command,100,stdin);
+		char command[100];    
+		fgets(command,100,stdin);   //user input  
 		if(strncmp(command,"exit",strlen("exit"))==0)
 		{
 			printf("Game ended by User!!!");
@@ -388,7 +389,7 @@ int main()
 
 		printBoard();
 		printf("\n");
-		if(winState())
+		if(winState())   //game won!!
 		{
 			printf("You have won the game!!!\n");
 			break;
